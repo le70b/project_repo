1a0d93995edb333ed91a6cc9b59d743631afdf75
CesarGoetgheluck <cesar.goetgheluck@gmail.com>
2025-04-13 15:04:52 +0200

    Create app_cesar.py and pink color

---
+++ app_cesar.py
@@ -0,0 +1,10 @@
+# Pink sidebar color
+pink_color = "#FFC0CB"
+
+st.markdown(f"""
+    <style>
+        [data-testid="stSidebar"] {{
+            background-color: {pink_color};
+        }}
+    </style>
+    """, unsafe_allow_html=True)
